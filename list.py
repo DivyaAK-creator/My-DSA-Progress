ffe7b6b5ea6e441788b82d89ec8c17ae0df83268
Divya A K <akdivya603@gmail.com>
2026-05-08 19:29:14 +0530

    updated

--- list.py
+++ list.py
@@ -15,3 +15,30 @@ nums = [1, 2, 2, 3, 1, 4]
 nums = list(set(nums)) #it give set in list set holds only unique value
 print(nums)
 
+#Merge Two Sorted Lists
+a = [1, 3, 5]
+b = [2, 4, 6]
+c = (a+b)
+c.sort()
+print(c)
+
+# Move Zeros to End
+nums = [1,0,2,0,1]
+non_zero =[]
+zero =[]
+
+for i in nums:
+    if i==0:
+        zero.append(i)
+    else:
+        non_zero.append(i)
+
+print(non_zero+zero)
+
+# Check Palindrome List
+nums = [1, 4, 3, 2, 1]
+
+if nums == nums[::-1]:
+    print("Palindrome")
+else:
+     print("Not Palindrome")
