cb526bc110d799de19bf72c8ca4ffcfd0f50b0b3
Divya A K <akdivya603@gmail.com>
2026-05-09 21:19:40 +0530

    3 Topics

--- list.py
+++ list.py
@@ -42,3 +42,29 @@ if nums == nums[::-1]:
     print("Palindrome")
 else:
      print("Not Palindrome")
+
+#Rotate List Left (Move the first element to the end)
+n = [1,2,3,4,5,6,7]
+original = n.copy()
+while True:
+    n = n[1:]+[n[0]]
+    print(n)
+    if n == original:
+     break
+
+# Find Pairs with Given Sum
+num = [1,2,3,4,5]
+target = 6
+
+for i in range(len(num)):
+    for j in range(i+1,len(num)):
+        if num[i]+num[j]==target:
+            print(num[i],num[j])
+
+# Flatten Nested List
+nums = [[1,2],[3,4],[5]]
+flat = []
+for sublist in nums:
+    for item in sublist:
+        flat.append(item)
+print(flat)
